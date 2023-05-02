f434dcd431853a38b8b6ba01fa31ff9fdb1f2061
瀬川 友仁 <t_segawa@exbrain.internal>
2023-05-02 10:52:04 +0900

    既存のコードをポリモーフィズムにリファクタリングする

--- src/polymorphism3/Main.java
+++ src/polymorphism3/Main.java
@@ -3,7 +3,8 @@ package polymorphism3;
 public class Main {
 
 	public static void main(String[] args) {
-				
+		
+		
 		PercentageCoupon coupon1 = new PercentageCoupon(0.3);
 		FixedCoupon coupon2 = new FixedCoupon(1000);
 		System.out.println(Casher.payment(5000, coupon1));
